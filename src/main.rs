e6ebac1dbfe8302840ddc4915c451e333f3317b0
watasuke102 <watasuke102@gmail.com>
2021-08-29 06:53:28 +0900

    update: 設定ファイルの読み込み

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use serde_json::Value;
+use std::fs::File;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("env.json")
+        .expect("cannot read `env.json`: did you create this file? try `cp sample-env.json env.json` and edit it.");
+    let setting: Value = serde_json::from_reader(BufReader::new(file)).unwrap();
+    println!("{}", setting["discord_token"]);
 }
